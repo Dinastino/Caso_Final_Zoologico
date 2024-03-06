badfa4dc18659484f623250de99aacc3e2c15d61
Carlos <corammun@myuax.com>
2024-03-06 11:09:40 +0100

    lol

--- src/main/java/org/example/Habitats.java
+++ src/main/java/org/example/Habitats.java
@@ -1,4 +1,37 @@
 package org.example;
+import java.util.Scanner;
 
 public class Habitats {
+    public Habitats() {
+        Scanner scanner = new Scanner(System.in);
+        String tipo;
+        String habitat;
+
+        System.out.println("Ingrese el tipo de habitat: ");
+        tipo = scanner.nextLine();
+
+        switch (tipo) {
+            case "terrestre":
+                System.out.println("Habitat terrestre");
+                habitat = "terrestre";
+                break;
+            case "acuatico":
+                System.out.println("Habitat acuatico");
+                habitat = "acuatico";
+                break;
+            case "aereo":
+                System.out.println("Habitat aereo");
+                habitat = "aereo";
+                break;
+            default:
+                System.out.println("Habitat desconocido");
+                break;
+        }
+
+    }
+
+    public int LmpiezaHabitat() {
+
+        return 0;
+    }
 }
