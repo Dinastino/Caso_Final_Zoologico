088fbabe9200cbbf04625d15d56c9520bf8e7c99
Carlos <corammun@myuax.com>
2024-03-11 18:42:58 +0100

    uhouadhsi

--- src/main/java/org/example/Habitats.java
+++ src/main/java/org/example/Habitats.java
@@ -16,9 +16,35 @@ public class Habitats {
         System.out.println("Area: " + area);
     }
 
+    public static Habitats crearHabitat() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la temperatura del habitat: ");
+        double temperatura = scanner.nextDouble();
+        System.out.println("Ingrese la humedad del habitat: ");
+        double humedad = scanner.nextDouble();
+        System.out.println("Ingrese el area del habitat: ");
+        double area = scanner.nextDouble();
+        Habitats habitat = new Habitats();
+        habitat.temperatura = temperatura;
+        habitat.humedad = humedad;
+        habitat.area = area;
+        return habitat;
+    }
+
 }
 class Acuatico extends Habitats {
+    double nivelAgua;
 
+    public Acuatico(double temperatura, double humedad, double area, double nivelAgua) {
+        super.crearHabitat();
+        this.nivelAgua = nivelAgua;
+    }
+
+
+    public void mostrarCondiciones() {
+        super.mostrarHabitat();
+        System.out.println("Nivel de agua: " + nivelAgua);
+    }
 }
 
 class Terrestre extends Habitats {
